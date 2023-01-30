7e616edc3b2ca59baf69ed5d137736db89f3544a
Lahbib Semlali <semllmes01@gmail.com>
2023-01-30 18:21:13 +0100

    3d but need fix

--- cub3d.h
+++ cub3d.h
@@ -1,10 +1,26 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# define UP 126
-# define DOWN 125
-# define RIGHT 124
-# define LEFT 123
+# define FOV 66
+# define WIDTH 1000
+# define HEIGHT 500
+
+# define UP 13
+# define DOWN 1
+# define RIGHT 2
+# define LEFT 0
+# define ROTR 124
+# define ROTL 123
+
+# define BLUE 0X0000FF
+# define RED 0XFF0000
+# define WHITE 0XFFFFFF
+# define GREEN 0X00FF00
+
+# define BLACK 0X000000
+# define PURPLE 0X800080
+# define ORANGE 0XFF5733
+# define YELLOW 0XF4D03F
 
 # include <stdio.h>
 # include <fcntl.h>
@@ -26,6 +42,9 @@ typedef struct s_win{
 typedef struct s_player{
 	t_win	*win;
 	char	**map;
+	double		turnx;
+	double		turny;
+	double 		rotangle;
 	int		x_idx;
 	int		y_idx;
 	int		x;
--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include "cub3d.h"
 
+double	degrees_to_radians(int fov)
+{
+	return (fov * M_PI / 180);
+}
+
 char	**read_map(int fd)
 {
 	char	s[1000];
@@ -7,6 +12,86 @@ char	**read_map(int fd)
 	read(fd, s, 1000);
 	return (ft_split(s, '\n'));
 }
+
+int draw_line(t_player *p, int tox, int toy, int color)
+{
+	double dx = tox - p->x;
+	double dy = toy - p->y;
+	int pxs = sqrt((dx * dx) + (dy * dy));
+	dx /= pxs;
+	dy /= pxs;
+	double px = p->x;
+	double py = p->y;
+	while (pxs)
+	{
+		mlx_pixel_put(p->win->mlx_p, p->win->mlx_w, px, py, color);
+		px += dx;
+		py += dy;
+		--pxs;
+	}
+	return (0);
+}
+
+void	ren3d(t_player *p)
+{
+	double ang = p->rotangle - FOV / 2;
+	t_player tmp = *p;
+	for (int i = 0; i < 1000; i++)
+	{
+		p->turnx = p->x;
+		p->turny = p->y;
+		while (p->map[(int)p->turny / 50][(int)p->turnx / 50] != '1')
+		{
+			p->turnx += cos(degrees_to_radians(ang));
+			p->turny += sin(degrees_to_radians(ang));
+		}
+		double dis = sqrt(pow(p->x - p->turnx, 2) + pow(p->y - p->turny, 2));
+		double wallh = (250 / dis) * 50;
+		tmp.x = i;
+		tmp.y = 0;
+		draw_line(&tmp, i, 250 - wallh, BLUE);
+		tmp.y = 250 - wallh;
+		draw_line(&tmp, i, 250 + wallh, YELLOW);
+		tmp.y = 250 + wallh;
+		draw_line(&tmp, i, 500, RED);
+		ang += 66.0 / 1000.0;
+	}
+}
+
+// void draw_line(t_player *p, int x2, int y2) {
+//     int dx = abs(x2 - p->x);
+//     int dy = abs(y2 - p->y);
+//     int x = p->x;
+//     int y = p->y;
+//     int sx = p->x < x2 ? 1 : -1;
+//     int sy = p->y < y2 ? 1 : -1;
+//     int err = dx - dy;
+
+//     while ((x != x2 || y != y2) && p->map[y / 50][x / 50] != '1') {
+//         mlx_pixel_put(p->win->mlx_p, p->win->mlx_w, x, y, 0x60ec1d); // put a white pixel at (x,y)
+//         int e2 = 2 * err;
+//         if (e2 > -dy) {
+//             err -= dy;
+//             x += sx;
+//         }
+//         if (e2 < dx) {
+//             err += dx;
+//             y += sy;
+//         }
+//     }
+//     mlx_pixel_put(p->win->mlx_p, p->win->mlx_w, x, y, 0x60ec1d); 
+// }
+
+// void	camera(t_player *p)
+// {
+// 	int k = p->turn;
+// 	while (k < p->turn * 50)
+// 	{
+// 		draw_line(p, p->win->mlx_p, p->win->mlx_w, p->x, p->y, 0, k);
+// 		k++;
+// 	}
+// 	p->turn += 50;
+// }
 
 void	create_wind(char **map, t_player *p)
 {
@@ -21,20 +106,23 @@ void	create_wind(char **map, t_player *p)
 		x = 0;
 		while (map[i][j])
 		{
-			if (map[i][j] == '1')
-				mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_1, x, y);
-			else if (map[i][j] == 'p')
+			// if (map[i][j] == '1')
+			// 	mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_1, x, y);
+			if (map[i][j] == 'p')
 			{
 				p->x = x;
 				p->y = y;
 				p->x_idx = j;
 				p->y_idx = i;
-				mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, x, y);
-				mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_p, x, y);
+				p->rotangle = 0;
+				p->turnx = x;
+				p->turny = y;
+				// mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, x, y);
+				// mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_p, x, y);
 			}
-			else {
-				mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, x, y);
-			}
+			// else {
+			// 	mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, x, y);
+			// }
 			j++;
 			x += 50;
 		}
@@ -47,32 +135,53 @@ void	move_player(int key, t_player *p)
 {
 	if (key == UP && p->map[(p->y - 10) / 50][p->x_idx] != '1')
 	{
-		printf("---- %d\n", p->y);
-		mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, (p->x / 50) * 50, p->y - (p->y % 50));
-		p->y -= 10;
-		mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_p, p->x, p->y);
+		// mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, (p->x / 50) * 50, p->y - (p->y % 50));
+		p->x += cos(degrees_to_radians(p->rotangle)) * 3;
+		p->y += sin(degrees_to_radians(p->rotangle)) * 3;
+		// mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_p, p->x, p->y);
+		p->x_idx = p->x / 50;
 		p->y_idx = p->y / 50;
+		ren3d(p);
 	}
 	if (key == DOWN && p->map[(p->y + 10) / 50][p->x_idx] != '1')
 	{
-		mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, (p->x / 50) * 50, p->y - (p->y % 50));
-		p->y += 10;
-		mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_p, p->x, p->y);
+		// mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, (p->x / 50) * 50, p->y - (p->y % 50));
+		p->x -= cos(degrees_to_radians(p->rotangle)) * 3;
+		p->y -= sin(degrees_to_radians(p->rotangle)) * 3;
+		// mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_p, p->x, p->y);
+		p->x_idx = p->x / 50;
 		p->y_idx = p->y / 50;
+		ren3d(p);
 	}
 	if (key == RIGHT && p->map[p->y_idx][(p->x + 10) / 50] != '1')
 	{
-		mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, (p->x / 50) * 50, p->y - (p->y % 50));
-		p->x += 10;
-		mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_p, p->x, p->y);
+		// mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, (p->x / 50) * 50, p->y - (p->y % 50));
+		p->x += sin(degrees_to_radians(p->rotangle)) * 3;
+		p->y += cos(degrees_to_radians(p->rotangle)) * 3;
+		// mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_p, p->x, p->y);
 		p->x_idx = p->x / 50;
+		p->y_idx = p->y / 50;
+		ren3d(p);
 	}
 	if (key == LEFT && p->map[p->y_idx][(p->x - 10) / 50] != '1')
 	{
-		mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, (p->x / 50) * 50, p->y - (p->y % 50));
-		p->x -= 10;
-		mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_p, p->x, p->y);
+		// mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_0, (p->x / 50) * 50, p->y - (p->y % 50));
+		p->x -= sin(degrees_to_radians(p->rotangle)) * 3;
+		p->y -= cos(degrees_to_radians(p->rotangle)) * 3;
+		// mlx_put_image_to_window(p->win->mlx_p, p->win->mlx_w, p->win->img_p, p->x, p->y);
 		p->x_idx = p->x / 50;
+		p->y_idx = p->y / 50;
+		ren3d(p);
+	}
+	if (key == ROTR)
+	{
+		p->rotangle += 3;
+		ren3d(p);
+	}
+	if (key == ROTL)
+	{
+		p->rotangle -= 3;
+		ren3d(p);
 	}
 }
 
@@ -97,7 +206,7 @@ int main()
 	win->height = 50;
 	win->width = 50;
 	win->mlx_p = mlx_init();
-	win->mlx_w = mlx_new_window(win->mlx_p, 1000, 500, "hello");
+	win->mlx_w = mlx_new_window(win->mlx_p, WIDTH, HEIGHT, "hello");
 	win->img_0 = mlx_xpm_file_to_image(win->mlx_p, "textures/black.xpm", &win->height, &win->width);
 	win->img_p = mlx_xpm_file_to_image(win->mlx_p, "textures/player.xpm", &p->height, &p->width);
 	win->img_1 = mlx_xpm_file_to_image(win->mlx_p, "textures/gray.xpm", &win->height, &win->width);
@@ -106,6 +215,7 @@ int main()
 	map = read_map(fd);
 	p->map = map;
 	create_wind(map, p);
+	ren3d(p);
 	mlx_hook(win->mlx_w, 2, 0, key_hook, p);
 	mlx_loop(win->mlx_p);
 }
