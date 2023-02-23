a293f7bb4afdbb632cd9a4444c833eb2950fd68a
RizQy Rahhal <rarahhal@student.1337.ma>
2023-02-23 16:55:29 +0100

    after validation

--- parser/ft_error.c
+++ parser/ft_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_error.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsemlali <lsemlali@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rarahhal <rarahhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 12:08:32 by rarahhal          #+#    #+#             */
-/*   Updated: 2023/02/22 16:54:43 by lsemlali         ###   ########.fr       */
+/*   Updated: 2023/02/23 16:54:02 by rarahhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void	ft_error(char *error)
 {
 	write(2, "Error\n", 6);
-	(void)error;
 	write(2, error, ft_strlen(error));
 	exit(EXIT_FAILURE);
 }
